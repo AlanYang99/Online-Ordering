20649680000ffb72bc65abdf53c275879c249e49
AlanYang99 <33969636+AlanYang99@users.noreply.github.com>
2019-04-07 01:25:54 +1100

    Add files via upload
    Made classes for other ingredients types (cheese,patties,etc)

--- Backend/mains3.py
+++ Backend/mains3.py
@@ -8,20 +8,16 @@ class Food:
     def __str__(self):
         return f'{self.name} : {self.amount}'
 
-
 #Can create another class similar to food, but amount is simply (less,normal,extra)
 #cause 2 lettuce sounds weird
 
 class Vegetables:
 
-    def __init__(self,lettuce = 0,tomato = 0): #lettuce and tomato should be ints
-        #self._lettuce = Food('lettuce',lettuce,0.5) #50cents per serving of lettuce
-        #self._tomato = Food('tomatoes',tomato,0.3)
+    def __init__(self,lettuce = 0,tomato = 0):
         self._ingredients = [Food('lettuce',lettuce,0.5),Food('tomatoes',tomato,0.3)]
 
     def addIngredients(self,vege,price):
         self._ingredients.append(Food(vege,0,price))
-
 
     def setIngredients(self,vege,amount):
         found = 0
@@ -31,22 +27,6 @@ class Vegetables:
                 i.amount = amount
         if(found is 0):
             print("Ingredient not found")
-
-
-
-    @property
-    def lettuce(self):
-        return self._lettuce.amount
-
-    @property
-    def tomato(self):
-        return self._tomato.amount
-
-    def setLettuce(self,amount):
-        self._lettuce.amount = amount
-
-    def setTomato(self,amount):
-        self._tomato.amount = amount
 
     def getPrice(self):
         cost = 0
@@ -61,8 +41,122 @@ class Vegetables:
             output += '\n'
         return output
 
+class Cheese:
 
-Veg = Vegetables(1,1)
-Veg.addIngredients('Pumpkin',0.6) #Assuming staff added new item
-Veg.setIngredients('Pumpkin',4) #Customer orders 4 serving of pumpkin
-print(Veg.getVegetables())
+    def __init__(self,cheddar = 0,feta = 0):
+        self._ingredients = [Food('cheddarcheese',cheddar,0.5),Food('fetacheese',feta,0.3)]
+
+    def addIngredients(self,cheese,price):
+        self._ingredients.append(Food(cheese,0,price))
+
+    def setIngredients(self,cheese,amount):
+        found = 0
+        for i in self._ingredients:
+            if(i.name is cheese):
+                found = 1
+                i.amount = amount
+        if(found is 0):
+            print("Ingredient not found")
+
+    def get_price(self):
+        cost = 0
+        for i in self._ingredients:
+            cost += i.amount * i.price
+        return cost
+
+    def get_cheese(self):
+        output = ''
+        for i in self._ingredients:
+            output+= i.__str__()
+            output += '\n'
+        return output
+
+class Sauce:
+
+    def __init__(self,tomato = 0,bbq = 0):
+        self._ingredients = [Food('tomatosauce',tomato,0.5),Food('bbqsauce',bbq,0.3)]
+
+    def addIngredients(self,cheese,price):
+        self._ingredients.append(Food(sauce,0,price))
+
+    def setIngredients(self,sauce,amount):
+        found = 0
+        for i in self._ingredients:
+            if(i.name is sauce):
+                found = 1
+                i.amount = amount
+        if(found is 0):
+            print("Ingredient not found")
+
+    def get_price(self):
+        cost = 0
+        for i in self._ingredients:
+            cost += i.amount * i.price
+        return cost
+
+    def get_sauce(self):
+        output = ''
+        for i in self._ingredients:
+            output+= i.__str__()
+            output += '\n'
+        return output
+
+class patties:
+
+    def __init__(self,beef = 0,chicken = 0):
+        self._ingredients = [Food('beefpatty',tomato,0.5),Food('chickenpatty',bbq,0.3)]
+
+    def addIngredients(self,patty,price):
+        self._ingredients.append(Food(patty,0,price))
+
+    def setIngredients(self,patty,amount):
+        found = 0
+        for i in self._ingredients:
+            if(i.name is patty):
+                found = 1
+                i.amount = amount
+        if(found is 0):
+            print("Ingredient not found")
+
+    def get_price(self):
+        cost = 0
+        for i in self._ingredients:
+            cost += i.amount * i.price
+        return cost
+
+    def get_sauce(self):
+        output = ''
+        for i in self._ingredients:
+            output+= i.__str__()
+            output += '\n'
+        return output
+
+class buns:
+
+    def __init__(self,sesame = 0,muffin = 0):
+        self._ingredients = [Food('sesamebun',sesame,0.5),Food('muffinbun',muffin,0.3)]
+
+    def addIngredients(self,bun,price):
+        self._ingredients.append(Food(bun,0,price))
+
+    def setIngredients(self,bun,amount):
+        found = 0
+        for i in self._ingredients:
+            if(i.name is bun):
+                found = 1
+                i.amount = amount
+        if(found is 0):
+            print("Ingredient not found")
+
+    def get_price(self):
+        cost = 0
+        for i in self._ingredients:
+            cost += i.amount * i.price
+        return cost
+
+    def get_sauce(self):
+        output = ''
+        for i in self._ingredients:
+            output+= i.__str__()
+            output += '\n'
+        return output
